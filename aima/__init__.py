33ab47a9a68fbda6b301a31b836d0f4ca24b093c
Hobson Lane <hobsonlane@gmail.com>
2014-08-31 22:03:56 -0700

    make version like pytz does it with year first (no 0.)

--- aima/__init__.py
+++ aima/__init__.py
@@ -19,7 +19,7 @@
 # THE SOFTWARE.
 """aima -- Artificial Intelligence, A Modern Approach, by Stuart Russell and Peter Norrvig"""
 
-__version__ = '0.2014.0'
+__version__ = '2014.0'
 __authors__ = [
     'Peter Norvig <peter.norvig@gmail.com>',
     'Withal <withal@gmail.com>',
